abfc89e179909b8327005da10530d601a83119de
TuanCR <tuancr.pt@gmail.com>
2025-01-30 14:28:11 +0700

    refactor: use normal parser for NonEmtptyString dont need io-ts

--- src/model/value-objects/NoneEmptyString.ts
+++ src/model/value-objects/NoneEmptyString.ts
@@ -1,9 +1,8 @@
-import { BaseExceptionTrait } from '@logic/exception.base';
-import { Either, flow, pipe } from '@logic/fp';
+import { BaseException, BaseExceptionTrait } from '@logic/exception.base';
+import { Either, flow } from '@logic/fp';
 import { PrimitiveVOTrait } from '@model/value-object.base';
 import { Brand } from '@type_util/index';
-import { NonEmptyString as IONonEmptyString } from 'io-ts-types';
-import { ValidationErr, ValidationErrTrait, ValidationTrait } from '..';
+import { ValidationErr } from '..';
 
 export const isEmptyStringMaxNLength =
   <T>(maxLength: number) =>
@@ -37,22 +36,21 @@ export const NEStringAuFn = {
   getTrait,
 };
 
-export type NonEmptyString = IONonEmptyString;
+export type NonEmptyString = Brand<string, 'NonEmptyString'>;
 
 export type NonEmptyStringMax10 = Brand<string, 'NonEmptyStringMax10'>;
 
 export type NonEmptyStringMax100 = Brand<string, 'NonEmptyStringMax100'>;
 
 const parseNEString = (s: unknown) =>
-  pipe(
-    IONonEmptyString.decode(s),
-    Either.mapLeft(() =>
+  Either.fromPredicate<string, BaseException>(
+    (s) => typeof s === 'string' && s.length > 0,
+    () =>
       BaseExceptionTrait.construct(
         'non empty string should have content',
         'INVALID_NON_EMPTY_STRING',
       ),
-    ),
-  );
+  )(s as NonEmptyString);
 
 export const NonEmptyStringTrait: PrimitiveVOTrait<
   NonEmptyString,
