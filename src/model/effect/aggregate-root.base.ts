dadd6096f12fa84aa0810f025c7a6e807c0943dc
TuanCR <tuancr.pt@gmail.com>
2025-04-14 18:32:43 +0700

    refactor: simplify AggGenericTrait by leveraging EntityGenericTrait

--- src/model/effect/aggregate-root.base.ts
+++ src/model/effect/aggregate-root.base.ts
@@ -1,17 +1,18 @@
 import { ReadonlyRecord } from 'effect/Record';
-import { Option, Effect } from 'effect';
-import { pipe } from 'effect/Function';
-import { ParseResult, Parser } from './validation';
+import { Option } from 'effect';
+import { ParseResult } from './validation';
 import { Entity, EntityTrait, IEntityGenericTrait } from './entity.base';
-import { Identifier } from 'src/typeclasses/obj-with-id';
-import { GetProps } from 'src/typeclasses';
 import { DomainEvent } from '../event/domain-event.base';
+import { EntityGenericTrait } from './entity.impl';
 
 /**
  * AggregateRoot type that extends Entity
  */
 export type AggregateRoot<
-  Props extends ReadonlyRecord<string, unknown> = ReadonlyRecord<string, unknown>
+  Props extends ReadonlyRecord<string, unknown> = ReadonlyRecord<
+    string,
+    unknown
+  >,
 > = Entity<Props> & {
   readonly domainEvents: ReadonlyArray<DomainEvent>;
 };
@@ -29,9 +30,10 @@ export type WithAggregateMetaInput<OriginInput> = OriginInput & {
 /**
  * Parser for aggregate root properties
  */
-export type AggregatePropsParser<A extends AggregateRoot = AggregateRoot, I = unknown> = (
-  raw: I
-) => ParseResult<A['props']>;
+export type AggregatePropsParser<
+  A extends AggregateRoot = AggregateRoot,
+  I = unknown,
+> = (raw: I) => ParseResult<A['props']>;
 
 /**
  * Interface for aggregate root trait
@@ -39,20 +41,24 @@ export type AggregatePropsParser<A extends AggregateRoot = AggregateRoot, I = un
 export interface AggregateRootTrait<
   A extends AggregateRoot,
   NewParams = unknown,
-  ParseParams = unknown
-> extends EntityTrait<A, NewParams, WithAggregateMetaInput<ParseParams>> {}
+  ParseParams = unknown,
+> extends EntityTrait<A, NewParams, ParseParams> {}
 
 /**
  * Generic aggregate root trait interface
  */
 export interface IAggGenericTrait extends IEntityGenericTrait {
-  getDomainEvents: <A extends AggregateRoot>(aggregate: A) => ReadonlyArray<DomainEvent>;
+  getDomainEvents: <A extends AggregateRoot>(
+    aggregate: A,
+  ) => ReadonlyArray<DomainEvent>;
   clearEvents: <A extends AggregateRoot>(aggregate: A) => A;
-  addDomainEvent: <A extends AggregateRoot>(event: DomainEvent) => (aggregate: A) => A;
+  addDomainEvent: <A extends AggregateRoot>(
+    event: DomainEvent,
+  ) => (aggregate: A) => A;
   createAggregateRootTrait: <A extends AggregateRoot, N = unknown, P = unknown>(
-    propsParser: AggregatePropsParser<A>,
+    propsParser: AggregatePropsParser<A, P>,
     tag: string,
-    options?: { autoGenId: boolean }
+    options?: { autoGenId: boolean },
   ) => AggregateRootTrait<A, N, P>;
 }
 
@@ -61,104 +67,28 @@ export interface IAggGenericTrait extends IEntityGenericTrait {
  */
 export const AggGenericTrait: IAggGenericTrait = {
   // Inherit all methods from EntityGenericTrait
-  getTag: (entity) => entity._tag,
-  getId: (entity) => entity.id,
-  getCreatedAt: (entity) => entity.createdAt,
-  getUpdatedAt: (entity) => entity.updatedAt,
-  markUpdated: (entity) => ({
-    ...entity,
-    updatedAt: Option.some(new Date())
-  }),
-  unpack: (entity) => entity.props as GetProps<typeof entity>,
-  isEqual: (entity1, entity2) => 
-    entity1._tag === entity2._tag && entity1.id === entity2.id,
-  
+  ...EntityGenericTrait,
   // Aggregate specific methods
   getDomainEvents: (aggregate) => aggregate.domainEvents,
-  
+
   clearEvents: (aggregate) => ({
     ...aggregate,
-    domainEvents: []
+    domainEvents: [],
   }),
-  
+
   addDomainEvent: (event) => (aggregate) => ({
     ...aggregate,
-    domainEvents: [...aggregate.domainEvents, event]
+    domainEvents: [...aggregate.domainEvents, event],
   }),
-  
-  // Factory methods
-  createEntityTrait: (propsParser, tag, options = { autoGenId: true }) => {
-    const parse = (input: any): ParseResult<any> => {
-      return pipe(
-        Effect.succeed(input),
-        Effect.flatMap((data) => {
-          const id = options.autoGenId && !data.id ? crypto.randomUUID() : data.id || '';
-          const createdAt = data.createdAt || new Date();
-          const updatedAt = data.updatedAt || Option.none();
-          
-          return pipe(
-            propsParser(data),
-            Effect.map((props) => ({
-              _tag: tag,
-              id: id as Identifier,
-              createdAt,
-              updatedAt,
-              props
-            }))
-          );
-        })
-      );
-    };
-    
-    return {
-      parse,
-      new: (params: any) => parse(params)
-    };
+  createAggregateRootTrait: <A extends AggregateRoot, N = unknown, P = unknown>(
+    propsParser: AggregatePropsParser<A, P>,
+    tag: string,
+    options?: { autoGenId: boolean },
+  ) => {
+    return EntityGenericTrait.createEntityTrait<A, N, P>(
+      propsParser,
+      tag,
+      options,
+    );
   },
-  
-  createAggregateRootTrait: (propsParser, tag, options = { autoGenId: true }) => {
-    const parse = (input: any): ParseResult<any> => {
-      return pipe(
-        Effect.succeed(input),
-        Effect.flatMap((data) => {
-          const id = options.autoGenId && !data.id ? crypto.randomUUID() : data.id || '';
-          const createdAt = data.createdAt || new Date();
-          const updatedAt = data.updatedAt || Option.none();
-          const domainEvents = data.domainEvents || [];
-          
-          return pipe(
-            propsParser(data),
-            Effect.map((props) => ({
-              _tag: tag,
-              id: id as Identifier,
-              createdAt,
-              updatedAt,
-              domainEvents,
-              props
-            }))
-          );
-        })
-      );
-    };
-    
-    return {
-      parse,
-      new: (params: any) => parse(params)
-    };
-  }
-};
-
-/**
- * Helper function to create an aggregate root trait
- */
-export const createAggregateRootTrait = <
-  A extends AggregateRoot,
-  N = unknown,
-  P = unknown
->(
-  propsParser: AggregatePropsParser<A>,
-  tag: string,
-  options?: { autoGenId: boolean }
-): AggregateRootTrait<A, N, P> => {
-  return AggGenericTrait.createAggregateRootTrait(propsParser, tag, options);
 };
