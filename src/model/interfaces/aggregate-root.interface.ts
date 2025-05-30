32d859ca9e5a60b8e6430895460172d2326b3ad4
TuanCR <tuancr.pt@gmail.com>
2025-05-30 14:25:30 +0700

    feat: reorganize aggregate-root code

--- src/model/interfaces/aggregate-root.interface.ts
+++ src/model/interfaces/aggregate-root.interface.ts
@@ -1,22 +1,27 @@
-import type { Effect } from '@effect/io/Effect';
-import type { Option } from '@effect/data/Option';
-import type { ReadonlyRecord } from '@effect/data/ReadonlyRecord';
-import type { BaseException } from '../exception.base';
-import type { IDomainEvent } from './domain-event.interface';
+import { ReadonlyRecord } from 'effect/Record';
+import { Effect, Option } from 'effect';
+import {
+  CommandOnModel,
+  Entity,
+  IEntityGenericTrait,
+} from './entity.interface';
+import { IDomainEvent } from './domain-event.interface';
+import { ParseResult } from './validation.interface';
+import { CoreException, EntityTrait } from '@model/effect';
+import { GetProps } from 'src/typeclasses';
 
 export type AggregateRoot<
   Props extends ReadonlyRecord<string, unknown> = ReadonlyRecord<
     string,
     unknown
   >,
-> = {
-  readonly id: string;
-  readonly createdAt: Date;
-  readonly updatedAt: Option.Option<Date>;
+> = Entity<Props> & {
   readonly domainEvents: ReadonlyArray<IDomainEvent>;
-  readonly props: Props;
 };
 
+/**
+ * Input type for aggregate root creation
+ */
 export type WithAggregateMetaInput<OriginInput> = OriginInput & {
   id?: string;
   createdAt?: Date;
@@ -24,24 +29,26 @@ export type WithAggregateMetaInput<OriginInput> = OriginInput & {
   domainEvents?: ReadonlyArray<IDomainEvent>;
 };
 
+/**
+ * Parser for aggregate root properties
+ */
 export type AggregatePropsParser<
   A extends AggregateRoot = AggregateRoot,
   I = unknown,
-> = (raw: I) => Effect<A['props'], BaseException, never>;
+> = (raw: I) => ParseResult<A['props']>;
 
+/**
+ * Interface for aggregate root trait
+ */
 export interface AggregateRootTrait<
   A extends AggregateRoot,
   NewParams = unknown,
   ParseParams = unknown,
-> {
-  new: (params: NewParams) => A;
-  parse: (params: ParseParams) => Effect<A, BaseException, never>;
-  getDomainEvents: (aggregate: A) => ReadonlyArray<IDomainEvent>;
-  clearEvents: (aggregate: A) => A;
-  addDomainEvent: (event: IDomainEvent) => (aggregate: A) => A;
-  addDomainEvents: (events: ReadonlyArray<IDomainEvent>) => (aggregate: A) => A;
-}
+> extends EntityTrait<A, NewParams, ParseParams> {}
 
+/**
+ * Generic aggregate root trait interface
+ */
 export interface IAggGenericTrait
   extends Omit<IEntityGenericTrait, 'asCommand'> {
   getDomainEvents: <A extends AggregateRoot>(
@@ -54,4 +61,21 @@ export interface IAggGenericTrait
   addDomainEvents: <A extends AggregateRoot>(
     events: ReadonlyArray<IDomainEvent>,
   ) => (aggregate: A) => A;
+  createAggregateRootTrait: <A extends AggregateRoot, N = unknown, P = unknown>(
+    propsParser: AggregatePropsParser<A, P>,
+    tag: string,
+    options?: { autoGenId: boolean },
+  ) => AggregateRootTrait<A, N, P>;
+  asCommand: <A extends AggregateRoot, I>(
+    reducerLogic: (
+      input: I,
+      props: GetProps<A>,
+      aggregate: A,
+      correlationId: string,
+    ) => Effect.Effect<
+      { props: GetProps<A>; domainEvents: IDomainEvent[] },
+      CoreException,
+      any
+    >,
+  ) => (input: I) => CommandOnModel<A>;
 }
