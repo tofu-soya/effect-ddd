193413e527205efa7a54a89745a9c3a463357c50
TuanCR <tuancr.pt@gmail.com>
2025-06-06 00:27:30 +0700

    feat: Date schema that allow Date native as decode value

--- src/model/value-object/date.ts
+++ src/model/value-object/date.ts
@@ -1,23 +1,33 @@
-import * as Schema from '@effect/schema/Schema';
-import { ParseResult } from '@effect/schema';
-import { Effect } from 'effect';
+import { Effect, ParseResult, Schema } from 'effect';
 
 export const DateSchema = Schema.transformOrFail(
-  Schema.union(Schema.string, Schema.instanceOf(Date)),
+  Schema.Union(Schema.String, Schema.instanceOf(Date)),
   Schema.instanceOf(Date),
   {
     decode: (input) => {
       try {
         const date = typeof input === 'string' ? new Date(input) : input;
-        return isNaN(date.getTime()) 
-          ? ParseResult.fail(new ParseResult.Type(Schema.literal('Date'), input, 'Invalid date format'))
+        return isNaN(date.getTime())
+          ? ParseResult.fail(
+              new ParseResult.Type(
+                Schema.Literal('Date').ast,
+                input,
+                'Invalid date format',
+              ),
+            )
           : ParseResult.succeed(date);
       } catch {
-        return ParseResult.fail(new ParseResult.Type(Schema.literal('Date'), input, 'Invalid date format'));
+        return ParseResult.fail(
+          new ParseResult.Type(
+            Schema.Literal('Date').ast,
+            input,
+            'Invalid date format',
+          ),
+        );
       }
     },
     encode: (date) => ParseResult.succeed(date),
-  }
+  },
 );
 
 export type DateVO = Schema.Schema.Type<typeof DateSchema>;
