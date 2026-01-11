28afc763dc1a241b18c901bbe16a4b50e6b57c70
tofu-soya <tuancr.pt@gmail.com>
2026-01-11 22:11:56 +0700

    feat: remove aim export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export * from './application';
 export * from './logic';
+
 export * from './model';
 export * from './ports';
 export * from './types';
 // export * as rabbitmq from './ports/pubsub/rabbitmq';
 export * from './serializer';
 export * from './ports/pubsub';
-export * from './aim';
 export * from './typeclasses';
 export * as typeorm from './ports/database/typeorm';
 export * as nestjs from './infra/nestjs';
